640ab84ab81b448096935ef60a9d0ee9701d8369
Kwosh Vick <kwoshvick@gmail.com>
2025-05-07 14:47:57 +0300

    config

--- config/api_urls.py
+++ config/api_urls.py
@@ -1,4 +1,4 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 
 urlpatterns = [
     path("accounts", include("apps.accounts.api.urls", namespace="accounts-api")),
